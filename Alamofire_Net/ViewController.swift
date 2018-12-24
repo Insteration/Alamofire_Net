73c47cb74b743fe167b2b4aa7222fe6d02fa8209
Artem Karmaz <inste@icloud.com>
2018-12-24 20:20:05 +0200

    Initial Commit

---
+++ Alamofire_Net/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alamofire_Net
+//
+//  Created by Artem Karmaz on 12/24/18.
+//  Copyright © 2018 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
